c43eef9a23c3a6bb8f50a4edd46e191d9c78795c
aabitbek <almazikay@gmail.com>
2022-01-13 13:38:06 +0600

    Change button

---
+++ lib/src/sreens/src_auth.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Авторизация'),
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CupertinoTextField(
+              placeholder: 'Логин или почта',
+            ),
+            CupertinoTextField(
+              placeholder: 'Пароль',
+            ),
+            SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: CupertinoButton.filled(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Text('Войти'),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(height: 19),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: CupertinoButton(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                color: Color(0xFF4631D2),
+                child: Text(
+                  'Зарегистрироваться',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
